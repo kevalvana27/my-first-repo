fe6b58adf9afdba80920764362c6b160851678da
kevalvana27 <vanagekeval27@gmail.com>
2026-02-26 14:24:13 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 class demo{
     public static void main (String [] args){
-        system.out.print("this is demo file for jenkins");
+        System.out.print("this is demo file for jenkins");
     }
 
 }
