e2485f935c71a6eaf67340db77dc771c52f815bd
kevalvana27 <vanagekeval27@gmail.com>
2026-03-05 14:30:19 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,7 +1,8 @@
 class Demo{
     public static void main (String [] args){
         System.out.print("this is demo file for keval");
-        System.out.print("this is demo file for keval2");
+        System.out.print("this is demo file for keval1");
+       
     }
 
 }
