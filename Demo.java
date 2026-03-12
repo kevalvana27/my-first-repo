331f7985f0bd2f6b8d8c5e5a26d8fb1b1ac57aab
kevalvana27 <vanagekeval27@gmail.com>
2026-03-12 13:00:19 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,9 +1,8 @@
 class Demo{
     public static void main (String [] args){
         System.out.println("Welcome keval");
-        System.out.println("this is file from git");
-        System.out.println("this is UPDATED file from git");
-       
+        System.out.println("this is Jenkins project");
+        
     }
 
 }
