d23729f219e6cc0156f6e2a3185dc9e6ff01a587
kevalvana27 <vanagekeval27@gmail.com>
2026-03-12 12:05:38 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,7 +1,9 @@
 class Demo{
     public static void main (String [] args){
         System.out.print("this is demo file for keval");
-        System.out.print("this is demo file for");
+        System.out.print("this is demo file for devops");
+        System.out.print("this is Updated for devops");
+        System.out.print("this is UPDATED for PIPLINE1_JOB");
        
     }
 
